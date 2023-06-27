9ec994e60467c8684c966a514f09d2fc24d039a1
MuathJ <muath.juady.1@gmail.com>
2023-06-27 06:30:17 -0500

    next config, popup & asset table

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Home from '../components/layout/Home'; 
+import SafeProvider from '@gnosis.pm/safe-apps-react-sdk';
 
 function App() {
     return (
-        <>
+        <SafeProvider>
             <Home />
-        </>
+        </SafeProvider>
     );
 }
 
