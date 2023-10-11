841768c7e320018fd261c52f9f5eba967c57f64b
oguzhanali <oguzxan.ali@gmail.com>
2023-10-11 16:49:49 +0300

    build: update project build configs

--- next.config.js
+++ next.config.js
@@ -1,27 +1,4 @@
-module.exports = {
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: [
-            {
-              key: "Access-Control-Allow-Origin",
-              value: '*',
-            },
-            {
-              key: "Access-Control-Allow-Methods",
-              value: 'GET',
-            },
-            {
-              key: "Access-Control-Allow-Headers",
-              value: 'X-Requested-With, content-type, Authorization',
-            },
-            {
-                key: 'Content-Security-Policy',
-                value: "frame-ancestors 'self' https://app.safe.global;",
-            },
-          ],
-        },
-      ]
-    },
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./app/**/*.{js,ts,jsx,tsx,mdx}",
-        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/**/*.{js,ts,jsx,tsx,mdx}",
-        './node_modules/tw-elements/dist/js/**/*.js'
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
       ],
     theme: {
         extend: {
-            screens: {
-                mobile: '500px',
-                '600px': '600px',
-                '700px': '700px',
-                '800px': '800px',
-                '1100px': '1100px',
-                '1200px': '1200px',
-                '1300px': '1300px',
-                '1500px': '1500px',
-                '1600px': '1600px',
-                tablet: '900px',
-                laptop: '1440px',
-            },
-            fontFamily: {
-                SSP: ['Source Sans Pro', 'sans-serif'],
-            },
             colors: {
                 primary: '#111827',
                 secondary: '#1f2937',
@@ -35,5 +16,5 @@ module.exports = {
             },
         },
     },
-    plugins: [require('tw-elements/dist/plugin')],
+    plugins: [require('@headlessui/tailwindcss')]
 };
