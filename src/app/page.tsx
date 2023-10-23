7371e55a526f5e2ae057aa6a88ac402e4c009182
oguzhanali <oguzxan.ali@gmail.com>
2023-10-23 16:29:39 +0300

    feat: add signout button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,33 @@ export default function Page() {
 
     async function checkAuth() {
         try {
-            const res = await fetch(backend + '/auth');
+            const res = await fetch('api/auth', { credentials: 'include' });
             if (res.status === 200) {
                 toast.success('Authorized');
-            } else toast.error('Not authorized');
-        } catch (e) {}
+            } else throw new Error('Not authorized');
+        } catch (e) {
+            toast.error(e instanceof Error ? e.message : 'Unknown error');
+        }
+    }
+    async function signout() {
+        try {
+            const res = await fetch('api/auth/signout', { credentials: 'include' });
+            if (res.status === 200) {
+                toast.success('Signed out');
+            } else throw new Error('Could not sign out');
+        } catch (e) {
+            toast.error(e instanceof Error ? e.message : 'Unknown error');
+        }
     }
 
     return (
-        <div className="flex h-16 flex-col items-center justify-center gap-6">
+        <div className="my-10 flex h-16 flex-col items-center justify-center gap-6 ">
             First page
             <button onClick={checkAuth} className=" rounded-lg bg-blue-500 px-6 py-3 text-white">
                 Check Auth
+            </button>
+            <button onClick={signout} className=" rounded-lg bg-blue-500 px-6 py-3 text-white">
+                Signout
             </button>
         </div>
     );
